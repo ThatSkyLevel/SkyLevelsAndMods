3a600bf8d82d0a9008cdb21e594508a54f5fcdb5
ERC <149953300+ERC-Innovations@users.noreply.github.com>
2024-03-31 18:50:08 -0400

    Add files via upload

---
+++ Levels/ThatSkyLevel/Resources.lua
@@ -0,0 +1,9 @@
+-- THIS FILE IS NOT AUTOMATICALLY GENERATED. DO MODIFY THIS FILE!
+-- Objects.level.bin
+resource "LevelObjects" "ThatSkyLevel" { level = "ThatSkyLevel", source = "Levels/ThatSkyLevel/Objects.level", stripDebug = true }
+
+-- BstBaked.meshes
+resource "LevelData" "ThatSkyLevel" { source = "Levels/ThatSkyLevel/BstBaked.meshes", lodToLoad = 0 }
+
+
+------- LEVEL CODE BEGINS HERE -------
